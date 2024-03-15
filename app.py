b554a38c586b6eff450258c7d448a65d7b19d912
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2024-03-15 12:47:52 -0700

    [add]: routes with ngrok

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, make_response, send_file
+from flask_ngrok import run_with_ngrok
+# pip install flask-ngrok
+# To run:
+# flask run --port=5002
+# ngrok http 5002
+# Install ngrok with homwbrew first and config auth token
+
+app = Flask(__name__)
+app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = False
+app.config['DEBUG'] = True
+
+# run_with_ngrok(app)
+
+@app.route('/')
+def index():
+    response_text = "Everything works"
+    status_code = 200  
+    response = make_response(response_text, status_code)
+    return response
+
+
+@app.route('/send-command')
+def send_command():
+    message_sent = False
+    
+    if message_sent:
+        response_text = "Message sent successfully"
+        status_code = 200  
+    else:
+        response_text = "Message not sent"
+        status_code = 501  
+
+    response = make_response(response_text, status_code)
+    return response 
+
+if __name__ == '__main__':
+    app.run()
